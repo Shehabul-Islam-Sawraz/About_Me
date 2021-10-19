7a33df79660be0031e25b8d83a3525c5fe7c5491
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-10-19 03:18:15 -0700

    Some functionalities are modified

---
+++ js/main.js
@@ -0,0 +1,83 @@
+jQuery(document).ready(function(){ 
+	
+	/* ---------------------------------------------------------------------- */
+	/*	Custom Functions
+	/* ---------------------------------------------------------------------- */
+
+	// Logo
+	var $logo 	= $('#logo');
+
+    if (location.href.indexOf("#") != -1) {
+        $logo.show();
+    }
+	// Show logo 
+	$('.menu .tabs a').click(function() {
+	  $logo.fadeIn('slow');
+	});
+	// Hide logo
+	$('.tab-profile').click(function() {
+	  $logo.fadeOut('slow');
+	});	
+
+	 
+	/* ---------------------------------------------------------------------- */
+	/*	Menu
+	/* ---------------------------------------------------------------------- */
+	
+	// Needed variables
+	var $content 		= $("#content");
+	
+	// Run easytabs
+  	$content.easytabs({
+	  animate			: true,
+	  updateHash		: false,
+	  transitionIn		:'slideDown',
+	  transitionOut		:'slideUp',
+	  animationSpeed	:600,
+	  tabs				:".tmenu",
+	  tabActiveClass	:'active',
+	});
+
+	
+	// Hover menu effect
+	$content.find('.tabs li a').hover(
+		function() {
+			$(this).stop().animate({ marginTop: "-7px" }, 200);
+		},function(){
+			$(this).stop().animate({ marginTop: "0px" }, 300);
+		}
+	);
+
+	// Menu Navigation
+	 $(".menu .tabs").carouFredSel({
+        responsive          : true,
+        direction           : "left",
+ 	    circular: false,
+    	infinite: false,
+        pagination  		: "#menu-controls",  
+        auto    			: false,
+        scroll 			: {
+            items           : 1,
+            duration        : 300,                        
+            wipe    : true
+        },
+		prev	: {	
+			button	: "#menu-prev",
+			key		: "right"
+		},
+		next	: { 
+			button	: "#menu-next",
+			key		: "left"
+		},
+	    swipe: {
+	        onTouch: true
+	    },
+        items: {
+            width: 140,
+            visible: {
+              min: 2,
+              max: 5
+            }
+        }           
+    });
+});	
